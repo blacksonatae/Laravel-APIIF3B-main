0323ae57fcde1e586c101bd2bde639642dee328f
chizururu <167151222+blacksonatae@users.noreply.github.com>
2024-10-17 18:56:09 +0700

    Update Prodi

--- routes/api.php
+++ routes/api.php
@@ -12,14 +12,15 @@ Route::get('/user', function (Request $request) {
 // Get
 Route::get('/fakultas', [FakultasController::class, 'index']);
 Route::get('/prodi', [ProdiController::class, 'index']);
-
-// Show
-Route::get('/fakultas/{fakultas}', [FakultasController::class, 'show']);
-
-
+// Post
 Route::post('/fakultas', [FakultasController::class, 'store']);
 Route::post('/prodi', [ProdiController::class, 'store']);
+// Show
+Route::get('/fakultas/{fakultas}', [FakultasController::class, 'show']);
+Route::get('/prodi/{prodi}', [ProdiController::class, 'show']);
+// Update
 Route::patch('/fakultas/{fakultas}', [FakultasController::class, 'update']);
 Route::patch('/prodi/{prodi}', [ProdiController::class, 'update']);
+// Delete
 Route::delete('/fakultas/{fakultas}', [FakultasController::class, 'destroy']);
 Route::delete('/prodi/{prodi}', [ProdiController::class, 'destroy']);
